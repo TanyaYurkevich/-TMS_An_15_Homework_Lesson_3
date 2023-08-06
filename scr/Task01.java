2e4b5be2a95c8cd99340c96a257f80864eb70982
TanyaYurkevich <141036160+TanyaYurkevich@users.noreply.github.com>
2023-08-07 01:07:34 +0300

    Update Task01.java

--- scr/Task01.java
+++ scr/Task01.java
@@ -1,7 +1,7 @@
 public class Task01 {
     public static void main(String[] args) {
         int a = 5;
-        int b = 5+3;
+        int b = a +3;
         int c = a + b;
 
         System.out.println ("a="+a + ", b="+b + ", c="+c);
